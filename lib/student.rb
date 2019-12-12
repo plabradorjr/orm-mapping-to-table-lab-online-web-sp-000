5bffe828754c90880e259cd840a681aeaf301591
Pancho Labrador <plabradorjr@gmail.com>
2019-12-12 04:36:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   attr_accessor :name, :grade
+  attr_reader :id 
 
   def initialize (name, grade, id = nil)
     @name =  name
     @grade = grade
-    @id = id 
+    @id = id
   end
 
 end
